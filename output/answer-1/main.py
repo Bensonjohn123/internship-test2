3943f3ff2ca428cf1befe9085f0a852e0060128a
Bensonjohn123 <98306679+Bensonjohn123@users.noreply.github.com>
2022-01-24 13:56:30 +0530

    Update main.py

--- output/answer-1/main.py
+++ output/answer-1/main.py
@@ -2,10 +2,6 @@ import numpy as np
 import pandas as pd
 result = pd.read_csv('/input/question1/main.csv')
 result.head()
-result.dtypes
-result.Year = pd.to_datetime(result.Year, format = '%Y')
-result.head()
-result.Year.dtypes
 result.set_index('Year', drop=True, inplace=True)
 result.head()
 result.drop(columns='Total', inplace=True)
