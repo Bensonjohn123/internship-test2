4b5eb76440eaff8af7b2600c52a71ccc12dcd8c3
Bensonjohn123 <98306679+Bensonjohn123@users.noreply.github.com>
2022-01-24 13:58:33 +0530

    Update main.py

--- output/answer-3/main.py
+++ output/answer-3/main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 ans = pd.read_csv('/input/question3/main.csv')
-ans.Team.nunique()
-ans.shape[1]
 result = ans[['Team', 'Yellow Cards', 'Red Cards']]
 result.sort_values(by=['Red Cards', 'Yellow Cards'], inplace = True)
 result
