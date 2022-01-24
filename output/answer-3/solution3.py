67472d184887603da957bbd7e5a93ae914927cf2
Bensonjohn123 <98306679+Bensonjohn123@users.noreply.github.com>
2022-01-24 14:22:28 +0530

    Update solution3.py

--- output/answer-3/solution3.py
+++ output/answer-3/solution3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-ans = pd.read_csv('/input/question-3/main.csv')
+ans = pd.read_csv('https://github.com/Bungeetech/internship-test2/blob/master/input/question-3/main.csv')
 result = ans[['Team', 'Yellow Cards', 'Red Cards']]
 result.sort_values(by=['Red Cards', 'Yellow Cards'], inplace = True)
 result
