547e9509cf9dfb0be6112663e424f5018f7c90ae
Bensonjohn123 <98306679+Bensonjohn123@users.noreply.github.com>
2022-01-24 14:20:41 +0530

    Update solution2.py

--- output/answer-2/solution2.py
+++ output/answer-2/solution2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-users = pd.read_csv('/input/question-3/main.csv', sep = '|')
+users = pd.read_csv('https://github.com/Bungeetech/internship-test2/blob/master/input/question-2/main.csv', sep = '|')
 print(users.groupby('occupation').age.min())
 print(users.groupby('occupation').age.max())
 
